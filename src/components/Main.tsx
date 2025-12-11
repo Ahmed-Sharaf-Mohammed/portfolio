177bf0fba4b04accd40aba8b921ddb6c029887c1
Ahmed Sharaf Mohammed Hassan <Ahmed.Sharaf.Mohammed.Hassan@gmail.com>
2025-12-11 19:23:52 +0200

    profile_img

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../assets/styles/Main.scss';
+import profilePic from "../assets/images/profile.jpg";
+
 
 function Main() {
 
@@ -9,7 +11,7 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://media.licdn.com/dms/image/v2/D4D03AQFhtH_ezmN0Dw/profile-displayphoto-scale_400_400/B4DZn2UN9oIcAg-/0/1760774120110?e=1767225600&v=beta&t=RMt6vakxZbir5pS4lZ9jXzPmRIVOl7GEV5icp36tTno" />
+          <img src={profilePic} alt="Ahmed Sharaf" className="profile-img" />
         </div>
         <div className="content">
           <div className="social_icons">
